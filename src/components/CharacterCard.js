a26886732b8c74df6ad3a5e6cfde7db8d2acdc7d
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-07-26 09:18:00 -0700

    Got name data pulling through to Char page, working on remaining fields

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,33 @@
 import React from 'react'
+import styled from "styled-components";
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
+const CardWrapper = styled.div`
+    border: 1px solid black;
+    border-radius: 5px;
+    width: 25vw;
+    max-width: 30vw;
+    margin: 1vw;
+    // height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around; 
+    background-color: whitesmoke;
+    opacity: 0.5;
+    padding: 1%;
+`;
+
+
+
+export default function CharacterCard (props) {
+  return (
+    <div>
+
+      <CardWrapper>
+        <h2> {props.name} </h2>
+      </CardWrapper>
+    
+      <span>todo: location</span>
+    
+    </div>
+  )
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,38 @@
 import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [charData, setCharData] = useState([])
 
-  useEffect(() => {
-    // TODO: Add AJAX/API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+  useEffect( () => {
+  // TODO: Add AJAX/API Request here - must run in `useEffect`
+  Axios.get("https://rickandmortyapi.com/api/character/"
+    ).then(res => {
+      setCharData(res.data.results)
+    })
+    .catch(res => {
+      alert ("There was an error retrieving data: ", res)})
+
+  //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, [])
 
-  return <section className='character-list grid-view'>
+  console.log("charData outside", charData)
 
+  return( 
+    <section className='character-list grid-view'>
       <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+      <div> 
+        
+        {console.log("charData inside return", charData)}
+        {charData.map(char => ( 
+          <CharacterCard 
+            name = {char.name}
+            key = {char.name}
+          />
+        ))}
+      </div>
+    </section>)
 
 }
